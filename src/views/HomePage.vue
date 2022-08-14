900f88c6bf3d9ce12f21b7aa7b740df8d5bd2d91
PeiYu <yupeiq1718@gmail.com>
2022-08-14 21:31:15 +0800

    feat: modify style & map

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,41 +1,46 @@
 <script setup lang="ts">
 type Type = 'staff'|'admin'
-
 const type = ref<Type>('staff')
+const setType = (value:Type) => {
+  type.value = value
+}
 
 type TypeMap = {
   [key in Type]?: string
 }
 
-const positionMap:TypeMap = {
-  staff: 'left-[8.33%] ',
-  admin: 'left-[8.33%] md:left-[41.67%] xl:left-[58.33%]'
+type StyleMap = {
+  [key:string]: TypeMap
 }
-
-const colorMap:TypeMap = {
-  staff: 'bg-primary',
-  admin: 'bg-secondary'
-}
-
-const backgroundMap:TypeMap = {
-  staff: 'bg-primary-light',
-  admin: 'bg-secondary-light'
-}
-
-const setType = (value:Type) => {
-  type.value = value
+const styleMap:StyleMap = {
+  position: {
+    staff: 'left-[8.33%] ',
+    admin: 'left-[8.33%] md:left-[41.67%] xl:left-[58.33%]'
+  },
+  background: {
+    staff: 'bg-primary',
+    admin: 'bg-secondary'
+  },
+  wrapper: {
+    staff: 'bg-primary-light',
+    admin: 'bg-secondary-light'
+  },
+  icon: {
+    staff: 'fill-primary',
+    admin: 'fill-secondary'
+  }
 }
 </script>
 
 <template>
   <div
     class="fixed w-full h-screen duration-1000"
-    :class="colorMap[type]"
+    :class="styleMap.background[type]"
   >
     <div class="absolute left-1/2 transform -translate-x-1/2 container h-full grid grid-cols-12 items-center">
       <div
-        class="absolute w-full h-[480px] rounded-2xl drop-shadow-md ease-linear duration-1000"
-        :class="backgroundMap[type]"
+        class="absolute w-full h-[480px]  hidden md:block rounded-2xl drop-shadow-md ease-linear duration-1000"
+        :class="styleMap.wrapper[type]"
       />
       <section
         class="absolute left-[8.33%] hidden md:block w-1/3 text-center"
@@ -45,7 +50,7 @@ const setType = (value:Type) => {
         </p>
         <BaseButton
           variant="muted"
-          class="bg-transparent text-lg"
+          class="bg-transparent text-md"
           @click="setType('staff')"
         >
           {{ '切換員工登入' }}
@@ -59,7 +64,7 @@ const setType = (value:Type) => {
         </p>
         <BaseButton
           variant="muted"
-          class="bg-transparent text-lg"
+          class="bg-transparent text-md"
           @click="setType('admin')"
         >
           {{ '切換管理員登入' }}
@@ -67,26 +72,48 @@ const setType = (value:Type) => {
       </section>
       <section
         class="absolute bg-white w-5/6 md:w-1/2 xl:w-1/3 h-[720px] rounded-2xl drop-shadow-md duration-1000 ease-in-out px-8 flex justify-evenly items-center flex-col"
-        :class="positionMap[type]"
+        :class="styleMap.position[type]"
       >
         <header>
           <BaseSvgIcon
             name="logo"
-            class="w-full fill-primary"
+            class="w-full duration-1000"
+            :class="styleMap.icon[type]"
           />
         </header>
-
         <article class="text-center text-dark">
           <h1 class="text-2xl font-bold mb-4">
-            光禾感知互評系統
+            {{ '光禾感知互評系統' }}
           </h1>
-          <p>員工登入</p>
+          <p v-if="type==='staff'">
+            {{ '員工登入' }}
+          </p>
+          <p v-if="type==='admin'">
+            {{ '管理員登入' }}
+          </p>
         </article>
-        <footer>
+        <footer class="text-center">
           <img
             src="@/assets/images/google.png"
             alt="google"
+            class="mb-4"
           >
+          <BaseButton
+            v-if="type==='admin'"
+            variant="secondary"
+            class="bg-transparent text-md md:hidden"
+            @click="setType('staff')"
+          >
+            {{ '切換員工登入' }}
+          </BaseButton>
+          <BaseButton
+            v-if="type==='staff'"
+            variant="primary"
+            class="bg-transparent text-md md:hidden"
+            @click="setType('admin')"
+          >
+            {{ '切換管理員登入' }}
+          </BaseButton>
         </footer>
       </section>
     </div>
