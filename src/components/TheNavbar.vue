064e7b03940d550a2722d48aa448d7fd4a3084ce
PeiYu <yupeiq1718@gmail.com>
2022-08-15 17:02:07 +0800

    feat: create TheNavbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+type Props = {
+  activeItem: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  activeItem: 'staff'
+})
+const items = ['staff', 'leader', 'result', 'calendar']
+
+const buttonStyle = computed(() => (item:string) => item === props.activeItem ? 'bg-primary fill-white' : 'bg-primary-light fill-primary')
+</script>
+
+<template>
+  <navbar class="w-[6rem] bg-primary-light rounded-2xl flex flex-col justify-start items-center">
+    <img
+      class="absolute top-4 rounded-full w-16 h-16"
+      src="@/assets/images/user.png"
+      alt="user"
+    >
+    <ul class="absolute top-24 bottom-24 border-y-2 border-primary py-8">
+      <li
+        v-for="item of items"
+        :key="item"
+        role="button"
+        :active="props.activeItem"
+        class="p-4 rounded-2xl w-16 h-16 flex flex-col justify-start items-center"
+        :class="buttonStyle(item)"
+      >
+        <BaseSvgIcon
+          class="w-8 h-8"
+          :name="item"
+        />
+      </li>
+    </ul>
+    <button class="absolute bottom-4 p-4 rounded-2xl w-[4.25rem] h-[4.25rem] flex flex-col justify-start items-center bg-white fill-primary">
+      <BaseSvgIcon
+        class="w-8 h-8"
+        name="logout"
+      />
+    </button>
+  </navbar>
+</template>
--- src/views/StaffPage.vue
+++ src/views/StaffPage.vue
@@ -1,3 +1,5 @@
 <template>
-  <div />
+  <div class="bg-primary w-full h-screen">
+    <TheNavbar class="absolute left-8 top-16 bottom-16" />
+  </div>
 </template>
