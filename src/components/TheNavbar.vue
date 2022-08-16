8ffdbdfe5e0131a5e378533ea1186819e8820fc8
PeiYu <yupeiq1718@gmail.com>
2022-08-16 16:32:40 +0800

    feat: add profile

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -16,19 +16,24 @@ const listStyle = computed(() => (name:string) => isFolded.value ? (name === act
 
 const pushRouter = (url:string) => router.push(url)
 
+const profile = {
+  name: '許斾旟',
+  position: '一般員工',
+  email: 'yupeiq1718@osensetech.com'
+}
 const isFolded = ref(true)
 const toggleIsFolded = () => {
   isFolded.value = !isFolded.value
 }
 
 const navBarStyle = computed(() => isFolded.value ? 'w-24' : 'w-[18rem]')
-
 const iconStyle = computed(() => isFolded.value ? 'w-8 h-8' : 'w-6 h-6')
-
 const titleStyle = computed(() => isFolded.value ? 'w-0' : 'px-4 w-48')
 
 const logoutButtonStyle = computed(() => isFolded.value ? 'py-4 w-16' : 'py-2 w-64')
 const logoutTitleStyle = computed(() => isFolded.value ? 'w-0' : 'px-4 w-24')
+
+const profileStyle = computed(() => isFolded.value ? 'w-0' : 'px-2 w-48')
 
 </script>
 
@@ -37,48 +42,68 @@ const logoutTitleStyle = computed(() => isFolded.value ? 'w-0' : 'px-4 w-24')
     class="bg-theme-light rounded-2xl flex flex-col items-start px-4 duration-300"
     :class="navBarStyle"
   >
-    <img
-      class="absolute top-4 rounded-full w-16 h-16"
-      src="@/assets/images/user.png"
-      alt="user"
-      @click="toggleIsFolded"
-    >
-    <ul class="absolute top-24 bottom-24 border-y-2 border-theme py-8">
-      <li
-        v-for="item of props.items"
-        :key="item.name"
-        role="button"
-        class="p-4 rounded-2xl flex justify-start items-start duration-300"
-        :class="listStyle(item.name)"
-        @click="pushRouter(item.url)"
+    <header class="absolute top-4 flex justify-start items-center">
+      <img
+        class="rounded-full w-16 h-16"
+        src="@/assets/images/user.png"
+        alt="user"
+        @click="toggleIsFolded"
+      >
+      <div
+        :class="profileStyle"
+        class="h-16 overflow-hidden"
+      >
+        <h2 class="text-lg font-bold text-dark">
+          {{ profile.name }}
+        </h2>
+        <h3 class="text-sm text-dark">
+          {{ profile.position }}
+        </h3>
+        <p class="text-xs text-muted">
+          {{ profile.email }}
+        </p>
+      </div>
+    </header>
+    <article>
+      <ul class="absolute top-24 bottom-24 border-y-2 border-theme py-8">
+        <li
+          v-for="item of props.items"
+          :key="item.name"
+          role="button"
+          class="p-4 rounded-2xl flex justify-start items-start duration-300"
+          :class="listStyle(item.name)"
+          @click="pushRouter(item.url)"
+        >
+          <BaseSvgIcon
+            :name="item.name"
+            :class="iconStyle"
+            class="duration-300"
+          />
+          <span
+            class="text-lg h-7 font-bold overflow-hidden duration-300"
+            :class="titleStyle"
+          >
+            <h2>{{ item.title }}</h2>
+          </span>
+        </li>
+      </ul>
+    </article>
+    <footer>
+      <button
+        class="absolute bottom-4 rounded-2xl p-4 flex justify-center items-center bg-white fill-theme text-theme duration-300"
+        :class="logoutButtonStyle"
       >
         <BaseSvgIcon
-          :name="item.name"
+          name="logout"
           :class="iconStyle"
-          class="duration-300"
         />
         <span
           class="text-lg h-7 font-bold overflow-hidden duration-300"
-          :class="titleStyle"
+          :class="logoutTitleStyle"
         >
-          <h2>{{ item.title }}</h2>
+          <h2>{{ '登出' }}</h2>
         </span>
-      </li>
-    </ul>
-    <button
-      class="absolute bottom-4 rounded-2xl p-4 flex justify-center items-center bg-white fill-theme text-theme duration-300"
-      :class="logoutButtonStyle"
-    >
-      <BaseSvgIcon
-        name="logout"
-        :class="iconStyle"
-      />
-      <span
-        class="text-lg h-7 font-bold overflow-hidden duration-300"
-        :class="logoutTitleStyle"
-      >
-        <h2>{{ '登出' }}</h2>
-      </span>
-    </button>
+      </button>
+    </footer>
   </nav>
 </template>
