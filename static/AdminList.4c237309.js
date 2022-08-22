bee1dac9c9820fed08e9b30eb51624da4dd6ade7
yupeiq1718 <yupeiq1718@users.noreply.github.com>
2022-08-22 07:08:25 +0000

    Deploying to gh-pages from @ yupeiq1718/peer-assessment@a15c116c65a3083ff25cdd54924f8b71daabe106 🚀

---
+++ static/AdminList.4c237309.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as r}from"./index.4529dba3.js";const n={};function t(c,o){return s(),r("div")}const a=e(n,[["render",t],["__file","/home/runner/work/peer-assessment/peer-assessment/src/views/AdminList.vue"]]);export{a as default};
--- static/AdminList.e022cd5b.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as r}from"./index.bbe5ce13.js";const n={};function t(c,o){return s(),r("div")}const a=e(n,[["render",t],["__file","/home/runner/work/peer-assessment/peer-assessment/src/views/AdminList.vue"]]);export{a as default};
