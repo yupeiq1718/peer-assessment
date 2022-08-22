7de06debf05d43d8048e1e6df80ddeb708943328
PeiYu <yupeiq1718@gmail.com>
2022-08-22 22:28:56 +0800

    feat: modify table padding

--- src/components/EmployeeStaffMain.vue
+++ src/components/EmployeeStaffMain.vue
@@ -23,41 +23,40 @@ const dataList = ref<Data[]>([{
 </script>
 
 <template>
-  <table class="relative border-separate border-spacing-x-0 border-spacing-y-4 text-dark text-center min-w-full">
+  <table class="relative border-separate border-spacing-x-0 border-spacing-y-2 text-dark text-center min-w-full">
     <thead>
-      <tr class="mb-4">
-        <td
+      <tr>
+        <th
           v-for="title of titleList"
           :key="title"
           class="px-4 py-2 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white font-bold"
         >
           {{ title }}
-        </td>
+        </th>
       </tr>
     </thead>
     <tbody>
       <tr
         v-for="data of dataList"
         :key="data.name"
+        class="whitespace-nowrap"
       >
-        <td class="px-8 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
-          <div class="flex flex-row items-center">
-            <img
-              class="rounded-full w-16 h-16 mr-4"
-              src="@/assets/images/user.jpg"
-              alt="user"
-            >
-            <span>
-              {{ data.name }}
-            </span>
-          </div>
+        <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white align-middle whitespace-nowrap">
+          <img
+            class="inline-block rounded-full w-16 max-w-none h-16 mr-4"
+            src="@/assets/images/user.jpg"
+            alt="user"
+          >
+          <span>
+            {{ data.name }}
+          </span>
         </td>
-        <td class="px-8 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
+        <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
           <BaseTag variant="theme">
             {{ data.department }}
           </BaseTag>
         </td>
-        <td class="px-8 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
+        <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
           <BaseRate
             v-for="(score, key) of data.scores"
             :key="key"
@@ -65,7 +64,7 @@ const dataList = ref<Data[]>([{
             :score="score"
           />
         </td>
-        <td class="px-8 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
+        <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
           <BaseSvgIcon
             role="button"
             class="w-6 h-6 m-2 fill-muted hover:fill-theme"
