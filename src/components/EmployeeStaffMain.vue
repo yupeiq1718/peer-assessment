b131b628637188c223e9a516ef314bd49f2c0439
PeiYu <yupeiq1718@gmail.com>
2022-08-24 12:08:18 +0800

    feat: add margin in TheMain

--- src/components/EmployeeStaffMain.vue
+++ src/components/EmployeeStaffMain.vue
@@ -23,60 +23,62 @@ const dataList = ref<Data[]>([{
 </script>
 
 <template>
-  <table class="relative border-separate border-spacing-x-0 border-spacing-y-2 text-dark text-center min-w-full">
-    <thead>
-      <tr>
-        <th
-          v-for="title of titleList"
-          :key="title"
-          class="px-4 py-2 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white font-bold"
-        >
-          {{ title }}
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="data of dataList"
-        :key="data.name"
-        class="whitespace-nowrap"
-      >
-        <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white align-middle whitespace-nowrap">
-          <img
-            class="inline-block rounded-full w-16 max-w-none h-16 mr-4"
-            src="@/assets/images/user.jpg"
-            alt="user"
+  <div class="mx-4">
+    <table class="relative border-separate border-spacing-x-0 border-spacing-y-2 text-dark text-center min-w-full">
+      <thead>
+        <tr>
+          <th
+            v-for="title of titleList"
+            :key="title"
+            class="px-4 py-2 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white font-bold"
           >
-          <span>
-            {{ data.name }}
-          </span>
-        </td>
-        <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
-          <BaseTag variant="theme">
-            {{ data.department }}
-          </BaseTag>
-        </td>
-        <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white w-2/3">
-          <BaseRate
-            v-for="(score, key) of data.scores"
-            :key="key"
-            variant="theme"
-            :score="score"
-          />
-        </td>
-        <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
-          <BaseSvgIcon
-            role="button"
-            class="w-6 h-6 m-2 fill-muted hover:fill-theme"
-            name="edit"
-          />
-          <BaseSvgIcon
-            role="button"
-            class="w-6 h-6 m-2 fill-muted hover:fill-theme"
-            name="delete"
-          />
-        </td>
-      </tr>
-    </tbody>
-  </table>
+            {{ title }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="data of dataList"
+          :key="data.name"
+          class="whitespace-nowrap"
+        >
+          <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white align-middle whitespace-nowrap">
+            <img
+              class="inline-block rounded-full w-16 max-w-none h-16 mr-4"
+              src="@/assets/images/user.jpg"
+              alt="user"
+            >
+            <span>
+              {{ data.name }}
+            </span>
+          </td>
+          <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
+            <BaseTag variant="theme">
+              {{ data.department }}
+            </BaseTag>
+          </td>
+          <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white w-2/3">
+            <BaseRate
+              v-for="(score, key) of data.scores"
+              :key="key"
+              variant="theme"
+              :score="score"
+            />
+          </td>
+          <td class="px-4 py-4 border-y-2 first:border-l-2 last:border-r-2 border-muted first:rounded-l-2xl last:rounded-r-2xl bg-white">
+            <BaseSvgIcon
+              role="button"
+              class="w-6 h-6 m-2 fill-muted hover:fill-theme"
+              name="edit"
+            />
+            <BaseSvgIcon
+              role="button"
+              class="w-6 h-6 m-2 fill-muted hover:fill-theme"
+              name="delete"
+            />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
