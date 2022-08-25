6f4069973c8a1b94a8aae7e38be035fa522f88f3
PeiYu <yupeiq1718@gmail.com>
2022-08-25 20:57:03 +0800

    feat: create FormTwo

--- src/components/EmployeeStaffUpdate.vue
+++ src/components/EmployeeStaffUpdate.vue
@@ -1,7 +1,18 @@
 <template>
   <article class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
-    <BaseFormOne title="合作部門" />
-    <BaseFormOne title="合作部門" />
+    <BaseFormOne
+      class="col-span-1"
+      title="合作部門"
+    />
+    <BaseFormOne
+      class="col-span-1"
+      title="合作部門"
+    />
+    <BaseFormTwo
+      class="col-span-1 lg:col-span-2 2xl:col-span-3"
+      title="問題一：你就本次與對方的合作經驗，綜合評分會給對方幾分"
+      tag="綜合"
+    />
   </article>
   <footer class="w-full flex justify-center">
     <BaseButton
