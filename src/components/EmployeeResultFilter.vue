26e2d0552d8d5c5265b864483ea6223c95514870
yupeiq1718 <66265207+yupeiq1718@users.noreply.github.com>
2022-08-27 18:26:32 +0800

    新增互評結果頁 (#27)
    * feat: create Result
    
    * feat: create Result Filter & Main
    
    * feat: modify TheSidebar icons
    
    * feat: modify Result Filter

---
+++ src/components/EmployeeResultFilter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <BaseFormOne
+      class="mb-4"
+      title="年度"
+    />
+    <BaseFormOne
+      class="mb-4"
+      title="次別"
+    />
+    <BaseFormOne
+      class="mb-4"
+      title="類型"
+    />
+  </div>
+</template>
