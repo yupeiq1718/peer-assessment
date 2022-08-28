052ea5df90aae72114b06d37e8d0a4a801594473
yupeiq1718 <66265207+yupeiq1718@users.noreply.github.com>
2022-08-29 02:31:04 +0800

    新增使用者設定頁 (#34)
    * feat: create AdminUser & UserMain
    
    * feat: create UsersFilter
    
    * feat: create UsersUpdate & UserCreate
    
    * feat: modify UserUPdate  grid

---
+++ src/components/BaseFormFour.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <form class="p-8 bg-white rounded-xl flex flex-col">
+    <label class="mb-4">{{ props.title }}</label>
+    <BaseInput
+      type="text"
+      class="max-w-[20rem]"
+    />
+  </form>
+</template>
---
+++ src/views/AdminUsers.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const isActive = ref(false)
+const modalType = ref('')
+const items = [
+  {
+    name: 'plus',
+    icon: 'plus',
+    function: () => {
+      isActive.value = true
+      modalType.value = 'create'
+    }
+  },
+  {
+    name: 'filter',
+    icon: 'filter',
+    function: () => {
+      isActive.value = true
+      modalType.value = 'filter'
+    }
+  }
+]
+
+</script>
+<template>
+  <div class="absolute w-full h-full">
+    <AdminUsersMain />
+    <transition
+      name="sidebar"
+      mode="out-in"
+      appear
+    >
+      <TheSideBar :items="items" />
+    </transition>
+    <transition
+      name="modal"
+      mode="out-in"
+    >
+      <TheModal
+        v-if="isActive"
+        v-model:is-active="isActive"
+        :size="modalType==='filter' ? 'side' : 'full'"
+      >
+        <AdminUsersUpdate v-if="modalType==='update'" />
+        <AdminUsersCreate v-if="modalType==='create'" />
+        <AdminUsersFilter v-if="modalType==='filter'" />
+      </TheModal>
+    </transition>
+  </div>
+</template>
