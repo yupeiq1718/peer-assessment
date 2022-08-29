b971c96cab564f921fcdc10559f5b2f60df028d5
PeiYu <yupeiq1718@gmail.com>
2022-08-29 18:15:38 +0800

    feat: add BaseAnswer button

--- src/components/BaseAnswerScore.vue
+++ src/components/BaseAnswerScore.vue
@@ -13,25 +13,41 @@ const setScore = (value:number) => {
 </script>
 
 <template>
-  <form class="p-8 bg-white rounded-xl flex flex-col items-start">
-    <label class="mb-4">
-      {{ props.title }}
-      <BaseTag variant="theme">
-        {{ props.tag }}
-      </BaseTag>
-    </label>
-    <div>
-      <div
-        v-for="number of 5"
-        :key="number"
-        class="inline-block"
-      >
-        <BaseScore
-          :variant="score===number ? 'theme' : 'muted'"
-          :score="number"
-          @click="setScore(number)"
-        />
+  <form class="p-8 bg-white rounded-xl">
+    <section class="flex flex-col">
+      <label class="mb-4">
+        {{ props.title }}
+        <BaseTag variant="theme">
+          {{ props.tag }}
+        </BaseTag>
+      </label>
+      <div class="mb-4">
+        <div
+          v-for="number of 5"
+          :key="number"
+          class="inline-block"
+        >
+          <BaseScore
+            :variant="score===number ? 'theme' : 'muted'"
+            :score="number"
+            @click="setScore(number)"
+          />
+        </div>
       </div>
-    </div>
+    </section>
+    <section class="flex flex-row">
+      <button class="rounded-2xl flex justify-center items-center bg-light hover:bg-theme fill-white w-12 h-12 mr-2 duration-500">
+        <BaseSvgIcon
+          name="edit"
+          class="w-8 h-8"
+        />
+      </button>
+      <button class="rounded-2xl flex justify-center items-center bg-light hover:bg-theme fill-white w-12 h-12 mr-2 duration-500">
+        <BaseSvgIcon
+          name="delete"
+          class="w-8 h-8"
+        />
+      </button>
+    </section>
   </form>
 </template>
