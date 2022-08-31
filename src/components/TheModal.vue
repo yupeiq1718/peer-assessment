cb0044e8a44ce3b058b1bf0dc69056553ec26159
PeiYu <yupeiq1718@gmail.com>
2022-08-31 20:36:08 +0800

    feat: add animation when TheMain leave

--- src/components/TheModal.vue
+++ src/components/TheModal.vue
@@ -18,11 +18,12 @@ const sizeMap = {
   full: 'w-full sm:w-[34rem] lg:w-[50rem] xl:w-[62rem] 2xl:w-[78rem]',
   side: 'max-w-full w-[26rem]'
 }
+
 </script>
 
 <template>
   <div
-    class="fixed top-0 md:top-8 bottom-0 md:bottom-8 right-0 md:right-0 xl:right-0 bg-theme-light border-2 border-theme rounded-l-xl shadow-md overflow-hidden z-30"
+    class="modal fixed top-0 md:top-8 bottom-0 md:bottom-8 right-0 md:right-0 xl:right-0 bg-theme-light border-2 border-theme rounded-l-xl shadow-md overflow-hidden z-30"
     :class="sizeMap[props.size]"
   >
     <article class="absolute top-4 bottom-17 md:bottom-4 inset-x-4 md:right-17 overflow-auto">
@@ -54,5 +55,11 @@ const sizeMap = {
 .modal-enter-from,
 .modal-leave-to {
   @apply transform translate-x-full;
+}
+
+.main-leave-to {
+  .modal {
+    @apply transform translate-x-full duration-1000;
+  }
 }
 </style>
