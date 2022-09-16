81b777c395bf12818fdad88af445cc852cfb68f5
PeiYu <yupeiq1718@gmail.com>
2022-09-16 19:01:15 +0800

    feat: add UsersFilters options

--- src/views/AdminUsersFilter.vue
+++ src/views/AdminUsersFilter.vue
@@ -1,6 +1,32 @@
 <script setup lang="ts">
+import { useUsers } from '@/store/users'
+
 const router = useRouter()
 const cancel = () => router.push('/admin/users')
+
+const departments = computed(() => useUsers().departments?.map(department => ({
+  value: department,
+  text: department
+})))
+
+const roles = [
+  {
+    value: 1,
+    text: '一般員工'
+  },
+  {
+    value: 2,
+    text: '一般主管'
+  },
+  {
+    value: 3,
+    text: '高級主管'
+  },
+  {
+    value: 4,
+    text: '管理員'
+  }
+]
 
 </script>
 
@@ -13,10 +39,12 @@ const cancel = () => router.push('/admin/users')
       <BaseFormSelect
         class="mb-4"
         title="部門"
+        :options="departments"
       />
       <BaseFormSelect
         class="mb-4"
         title="角色"
+        :options="roles"
       />
     </div>
   </TheModal>
