8e6f627f09aa036919f737b1e7b677c70c043d50
PeiYu <yupeiq1718@gmail.com>
2022-09-21 11:17:25 +0800

    feat: create BaseAnswer

--- src/components/AdminQuestionMain.vue
+++ src/components/AdminQuestionMain.vue
@@ -1,49 +1,16 @@
 <script setup lang="ts">
-import { getDepartments } from '@/utilities/data'
+import { useQuestions } from '@/store/questions'
 
-const departments = computed(() => getDepartments().map(department => ({
-  value: department,
-  text: department
-})))
+const questions = computed(() => useQuestions().questions)
 </script>
 
 <template>
   <article class="mx-5 my-2 grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
-    <BaseFormSelect
-      class="col-span-1"
-      title="合作部門"
-      name="department"
-      :options="departments"
-    />
-    <BaseFormSelect
-      class="col-span-1"
-      name="name"
-      title="合作對象"
-      :options="[
-        {
-          value: 'pey',
-          text: '許斾旟'
-        },
-        {
-          value: 'claire',
-          text: '劉于瑄'
-        }
-      ]"
-    />
-    <BaseAnswerScore
+    <BaseAnswer
+      v-for="question of questions"
+      :key="question.id"
       class="col-span-1 lg:col-span-2 2xl:col-span-3"
-      title="問題一：你就本次與對方的合作經驗，綜合評分會給對方幾分"
-      tag="綜合"
-    />
-    <BaseAnswerAll
-      class="col-span-1 lg:col-span-2 2xl:col-span-3"
-      title="問題一：你就本次與對方的合作經驗，綜合評分會給對方幾分"
-      description="對於信賴程度，是否有想補充地方"
-      tag="綜合"
-    />
-    <BaseAnswerText
-      class="col-span-1 lg:col-span-2 2xl:col-span-3"
-      title="最後，總體來說，就這次的合作經驗，是否有想補充的地方"
+      :question="question"
     />
   </article>
 </template>
---
+++ src/components/BaseAnswer.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+type Question = {
+  content: string,
+  id: string,
+  isRequired: boolean,
+  tag: string,
+  textHint: string,
+  typeId: number,
+}
+
+interface Props {
+  question: Question
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <form class="p-8 bg-white rounded-xl">
+    <section class="flex flex-col mb-4">
+      <label class="mb-2">
+        {{ props.question.content }}
+        <BaseTag variant="theme">
+          {{ props.question.tag }}
+        </BaseTag>
+      </label>
+      <BaseFormScore
+        v-if="props.question.typeId===1 || props.question.typeId===2"
+        class="mb-4"
+      />
+      <BaseFormTextarea
+        v-if="props.question.typeId===2 || props.question.typeId===3"
+        name="text"
+        :placeholder="props.question.textHint"
+        class="w-full"
+      />
+    </section>
+    <section class="flex flex-row">
+      <button class="rounded-2xl flex justify-center items-center bg-light hover:bg-theme fill-white w-12 h-12 mr-2 duration-500">
+        <BaseSvgIcon
+          name="edit"
+          class="w-8 h-8"
+        />
+      </button>
+      <button class="rounded-2xl flex justify-center items-center bg-light hover:bg-theme fill-white w-12 h-12 mr-2 duration-500">
+        <BaseSvgIcon
+          name="delete"
+          class="w-8 h-8"
+        />
+      </button>
+    </section>
+  </form>
+</template>
