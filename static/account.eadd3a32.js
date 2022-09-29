222e74d061cb092da78f947d8b0673eb9699fb48
yupeiq1718 <yupeiq1718@users.noreply.github.com>
2022-09-29 14:24:51 +0000

    Deploying to gh-pages from @ yupeiq1718/peer-assessment@ec621843927be1d397c23593d45d49ef347d4b12 🚀

--- static/account.eadd3a32.js
+++
@@ -1,1 +0,0 @@
-import{x as o,r as c}from"./index.a07c9b9e.js";import{u as s}from"./api.9ad47dd7.js";const d=o("account",()=>{const e=c();return{accountId:e,readAccountId:async t=>{try{const r=await s.get(`/user/${t}`);return e.value=r.data.data.userId,Promise.resolve(r)}catch(r){return Promise.reject(r)}}}});export{d as u};
