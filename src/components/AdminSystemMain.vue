ff9da85672d3a25f5ea199eedc256d79a3a5759c
PeiYu <yupeiq1718@gmail.com>
2022-09-29 17:25:26 +0800

    feat: modify finish & begin button

--- src/components/AdminSystemMain.vue
+++ src/components/AdminSystemMain.vue
@@ -4,71 +4,6 @@ import { systemStatus } from '@/utilities/data'
 
 const systemStatusId = computed(() => useSystem().systemStatus)
 
-type ToastData = {
-  isActive: boolean,
-  variant: string,
-  message: string
-}
-
-const setToastData:(data:ToastData) => void = inject('setToastData', () => null)
-
-const setIsLoading:(value:boolean) => void = inject('setIsLoading', () => null)
-
-type ConfirmData = {
-  isActive: boolean,
-  confirm: unknown,
-  text: string
-}
-const setConfirmData:(data:ConfirmData) => void = inject('setConfirmData', () => null)
-
-const handleAssessmentFinish = () => {
-  setConfirmData({
-    isActive: true,
-    confirm: () => setSystemStatus(1),
-    text: '請確認是否要結束互評？'
-  })
-}
-
-const handleAssessmentBegin = () => {
-  setConfirmData({
-    isActive: true,
-    confirm: () => setSystemStatus(2),
-    text: '請確認是否要開始互評？'
-  })
-}
-
-const getSystemStatus = async () => {
-  try {
-    const response = await useSystem().readSystemStatus()
-    console.log(response)
-  } catch ({ response }) {
-    console.log(response)
-  }
-}
-
-const setSystemStatus = async (status:number) => {
-  try {
-    setIsLoading(true)
-    const response = await useSystem().updateSystemStatus(status)
-    console.log(response)
-    await getSystemStatus()
-    setToastData({
-      isActive: true,
-      variant: 'success',
-      message: '更新成功'
-    })
-  } catch ({ response }) {
-    console.log(response)
-    setToastData({
-      isActive: true,
-      variant: 'error',
-      message: '更新失敗'
-    })
-  } finally {
-    setIsLoading(false)
-  }
-}
-
 </script>
 
 <template>
@@ -78,20 +13,6 @@ const setSystemStatus = async (status:number) => {
         <span class="text-dark">{{ '互評狀態' }}：</span>
         <span class="text-theme">{{ systemStatus[systemStatusId - 1].text }}</span>
       </h2>
-      <BaseButton
-        v-if="systemStatusId === 2"
-        variant="theme"
-        @click="handleAssessmentFinish"
-      >
-        {{ '結束互評' }}
-      </BaseButton>
-      <BaseButton
-        v-if="systemStatusId === 1"
-        variant="theme"
-        @click="handleAssessmentBegin"
-      >
-        {{ '開始互評' }}
-      </BaseButton>
     </header>
     <article class="grid grid-cols-1 lg:grid-cols-2">
       <section class="bg-white rounded-2xl px-6 py-4 m-4 mr-2">
--- src/views/AdminSystem.vue
+++ src/views/AdminSystem.vue
@@ -3,12 +3,85 @@ import { useSystem } from '@/store/system'
 
 const systemStatus = computed(() => useSystem().systemStatus)
 
+const items = computed(() => {
+  switch (systemStatus.value) {
+    case 1 :
+      return [{
+        name: 'begin',
+        icon: 'begin',
+        function: handleAssessmentBegin
+      }]
+    case 2 :
+      return [{
+        name: 'finish',
+        icon: 'finish',
+        function: handleAssessmentFinish
+      }]
+  }
+})
+
 const getSystemStatus = async () => {
   try {
     const response = await useSystem().readSystemStatus()
     console.log(response)
   } catch ({ response }) {
     console.log(response)
+  }
+}
+
+type ToastData = {
+  isActive: boolean,
+  variant: string,
+  message: string
+}
+
+const setToastData:(data:ToastData) => void = inject('setToastData', () => null)
+
+const setIsLoading:(value:boolean) => void = inject('setIsLoading', () => null)
+
+type ConfirmData = {
+  isActive: boolean,
+  confirm: unknown,
+  text: string
+}
+const setConfirmData:(data:ConfirmData) => void = inject('setConfirmData', () => null)
+
+const handleAssessmentFinish = () => {
+  setConfirmData({
+    isActive: true,
+    confirm: () => setSystemStatus(1),
+    text: '請確認是否要結束互評？'
+  })
+}
+
+const handleAssessmentBegin = () => {
+  setConfirmData({
+    isActive: true,
+    confirm: () => setSystemStatus(2),
+    text: '請確認是否要開始互評？'
+  })
+}
+
+const setSystemStatus = async (status:number) => {
+  try {
+    setIsLoading(true)
+    const response = await useSystem().updateSystemStatus(status)
+    console.log(response)
+    await getSystemStatus()
+    setToastData({
+      isActive: true,
+      variant: 'success',
+      message: '更新成功'
+    })
+  } catch ({ response }) {
+    console.log(response)
+    setToastData({
+      isActive: true,
+      variant: 'error',
+      message: '更新失敗'
+    })
+  } finally {
+    setIsLoading(false)
   }
 }
 
@@ -20,4 +93,11 @@ onBeforeMount(async () => {
   <div class="absolute w-full h-full">
     <AdminSystemMain v-if="systemStatus" />
   </div>
+  <transition
+    name="sidebar"
+    mode="out-in"
+    appear
+  >
+    <TheSideBar :items="items" />
+  </transition>
 </template>
