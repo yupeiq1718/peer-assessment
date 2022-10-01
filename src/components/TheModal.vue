cf65db315c4e6bbc974adbe13cc118dee5bc4bdf
PeiYu <yupeiq1718@gmail.com>
2022-10-01 20:36:52 +0800

    feat: modify TheModal padding

--- src/components/TheModal.vue
+++ src/components/TheModal.vue
@@ -26,7 +26,7 @@ const sizeMap = {
     class="modal fixed top-0 md:top-8 bottom-0 md:bottom-8 right-0 md:right-0 xl:right-0 bg-theme-light border-2 border-theme rounded-l-2xl shadow-md overflow-hidden z-30 duration-1000"
     :class="sizeMap[props.size]"
   >
-    <article class="absolute top-4 bottom-16 md:bottom-4 inset-x-4 md:right-16 overflow-auto">
+    <article class="absolute top-4 bottom-16 md:bottom-4 inset-x-4 md:right-18 overflow-auto">
       <slot />
     </article>
     <aside class="absolute right-3 bottom-1 w-full md:w-12 flex flex-row md:flex-col items-center justify-center">
