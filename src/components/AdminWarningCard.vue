c82d21083cbd497d16512007567bd29bde16abe4
PeiYu <yupeiq1718@gmail.com>
2022-10-02 02:33:24 +0800

    feat: add warningUser

--- src/components/AdminWarningCard.vue
+++ src/components/AdminWarningCard.vue
@@ -1,12 +1,35 @@
+<script setup lang="ts">
+interface Props {
+  id: number
+  name: string,
+  picture?: string,
+  count1: number,
+  count2: number
+}
+
+const props = defineProps<Props>()
+</script>
+
 <template>
-  <div class="rounded-2xl bg-white flex flex-col justify-center items-center p-8 shadow-md">
-    <img
-      class="inline-block rounded-full w-16 max-w-none h-16 mb-4"
-      src="/user.png"
-      alt="user"
-    >
-    <h2>
-      {{ '許斾旟' }}
-    </h2>
+  <div class="rounded-2xl bg-white flex flex-col justify-center items-center p-4 shadow-md">
+    <header class="flex justify-start items-center">
+      <img
+        class="inline-block rounded-full w-16 max-w-none h-16 bg-light mr-4"
+        :src="props.picture ||'/user.png'"
+        alt="user"
+      >
+      <span class="text-dark font-bold">
+        {{ props.name }}
+      </span>
+    </header>
+    <hr class="border-1 border-muted my-4 w-full">
+    <article class="text-dark">
+      <h2 v-if="props.count1">
+        {{ '尚有 ' }}<span class="text-theme font-bold">{{ props.count1 }}</span>{{ ' 筆員工互評未填' }}
+      </h2>
+      <h2 v-if="props.count2">
+        {{ '尚有 ' }}<span class="text-theme font-bold">{{ props.count2 }}</span>{{ ' 筆主管互評未填' }}
+      </h2>
+    </article>
   </div>
 </template>
--- src/components/AdminWarningMain.vue
+++ src/components/AdminWarningMain.vue
@@ -1,8 +1,32 @@
+<script setup lang="ts">
+import { useAnswers } from '@/store/answers'
+
+const warningUserList = computed(() => useAnswers().warningUserList)
+
+const getWarningUserList = async () => {
+  try {
+    const response = await useAnswers().readWarningUserList()
+    console.log(response)
+  } catch ({ response }) {
+    console.log(response)
+  }
+}
+
+onBeforeMount(async () => {
+  await getWarningUserList()
+})
+</script>
+
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
     <AdminWarningCard
-      v-for="number of 0"
-      :key="number"
+      v-for="warningUser of warningUserList"
+      :id="warningUser.id"
+      :key="warningUser.id"
+      :name="warningUser.name"
+      :picture="warningUser.picture"
+      :count1="warningUser.q1_unfilled_count"
+      :count2="warningUser.q2_unfilled_count"
       class="m-4"
     />
   </div>
