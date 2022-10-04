e479986fbe570a04200f7b2154eaa12a45370c83
PeiYu <yupeiq1718@gmail.com>
2022-10-04 12:26:51 +0800

    feat: add empty hint

--- src/components/AdminQuestionMain.vue
+++ src/components/AdminQuestionMain.vue
@@ -16,4 +16,10 @@ const questions = computed(() => useQuestions().questions(roleId.value) || [])
     :index="index"
     class="col-span-1 lg:col-span-2 2xl:col-span-3"
   />
+  <div
+    v-if="!questions.length"
+    class="px-3 2xl:px-4 py-8 rounded-2xl bg-white text-center align-middle whitespace-nowrap col-span-1 sm:col-span-2 lg:col-span-3 xl:col-span-4 2xl:col-span-5"
+  >
+    {{ '暫無資料' }}
+  </div>
 </template>
