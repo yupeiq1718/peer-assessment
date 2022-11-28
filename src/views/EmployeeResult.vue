97318a17068af64669bda6599838704e3c731fb2
PeiYu <yupeiq1718@gmail.com>
2022-11-28 11:48:55 +0800

    feat: add result main

--- src/views/EmployeeResult.vue
+++ src/views/EmployeeResult.vue
@@ -1,13 +1,102 @@
 <script setup lang="ts">
+import { useHistory } from '@/store/history'
+import { useAccount } from '@/store/account'
+
+const accountId = computed(() => useAccount().accountId)
+
 const router = useRouter()
 
+interface HistoryFilterData {
+  year?: number,
+  filename?: string,
+  roleId?: number,
+  managerId?: number
+}
+const historyFilterData = ref<HistoryFilterData>()
+const setHistoryFilterData = (value:HistoryFilterData) => {
+  historyFilterData.value = value
+}
+provide('historyFilterData', historyFilterData)
+provide('setHistoryFilterData', setHistoryFilterData)
+
 const items = [
+  {
+    name: '下載',
+    icon: 'download'
+    // function: () => readAllHistoryReport({
+    //   year: historyFilterData.value?.year || NaN,
+    //   filename: historyFilterData.value?.filename || '',
+    //   roleId: historyFilterData.value?.roleId || NaN
+    // })
+  },
   {
     name: '篩選',
     icon: 'filter',
     function: () => router.push('/employee/result/filter')
   }
 ]
+
+const setIsLoading:(value:boolean) => void = inject('setIsLoading', () => null)
+
+const readHistory = async () => {
+  try {
+    setIsLoading(true)
+    const response = await useHistory().readHistory()
+    console.log(response)
+  } catch (error) {
+    console.log(error)
+  } finally {
+    setIsLoading(false)
+  }
+}
+
+const readHistoryScore = async ({ year, filename, roleId, managerId }: {
+  year: number, filename: string, roleId: number, managerId: number
+}) => {
+  try {
+    setIsLoading(true)
+    const response = await useHistory().readHistoryScore({
+      year: Number(year),
+      filename: filename,
+      roleId: Number(roleId),
+      managerId: Number(managerId)
+    })
+    console.log(response)
+  } catch (error) {
+    console.log(error)
+  } finally {
+    setIsLoading(false)
+  }
+}
+
+// const readHistoryReport = async ({ year, filename, roleId }: {
+//   year: number, filename: string, roleId: number
+// }) => {
+//   try {
+//     setIsLoading(true)
+//     const response = await useHistory().readAllHistoryReport({
+//       year: Number(year),
+//       filename: filename,
+//       roleId: Number(roleId)
+//     })
+//     console.log(response)
+//   } catch (error) {
+//     console.log(error)
+//   } finally {
+//     setIsLoading(false)
+//   }
+// }
+
+watch(() => historyFilterData.value, () => readHistoryScore({
+  year: historyFilterData.value?.year || NaN,
+  filename: historyFilterData.value?.filename || '',
+  roleId: historyFilterData.value?.roleId || NaN,
+  managerId: accountId.value || NaN
+}))
+
+onBeforeMount(async () => {
+  await readHistory()
+})
 
 </script>
 <template>
